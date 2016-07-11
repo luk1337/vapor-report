60987295020ec37197bbc6f61759fbd1b2d9ecd6
LuK1337 <priv.luk@gmail.com>
2016-07-11 19:55:40 +0200

    Use `find` in async scripts as well

--- commendAsync.sh
+++ commendAsync.sh
@@ -10,7 +10,7 @@ fi
 
 node protos/updater.js
 
-for user in `ls users/[!example]*.json`; do
+for user in `find users -type f -not -name example.json`; do
     session_name="vapor-commend_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
     command="node commend.js $user $1"
     screen -dmS $session_name $command
