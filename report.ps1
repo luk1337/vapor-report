acf19d17d3b43615f8065fb137b77a45a7891fda
LuK1337 <priv.luk@gmail.com>
2016-07-17 19:32:08 +0200

    Handle multi-ID in Windows script

--- report.ps1
+++ report.ps1
@@ -1,6 +1,6 @@
 $counter = 0
 $accounts = (Get-ChildItem .\users\*.json -Exclude example.json).Count
-$steamId = Read-Host -Prompt 'Enter the steamID64 you want to report'
+$steamId = Read-Host -Prompt 'Enter the steamID64s you want to report'
 
 node protos/updater.js
 
