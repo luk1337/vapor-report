dee6615f485ea0696c2807fa67f212dbf517fe4a
LuK1337 <priv.luk@gmail.com>
2016-07-18 12:11:36 +0200

    Do not enforce having .json extension on Windows

--- report.ps1
+++ report.ps1
@@ -1,10 +1,10 @@
 $counter = 0
-$accounts = (Get-ChildItem .\users\*.json -Exclude example.json).Count
+$accounts = (Get-ChildItem .\users\* -Exclude example.json).Count
 $steamId = Read-Host -Prompt 'Enter the steamID64s you want to report'
 
 node protos/updater.js
 
-foreach ($account in $(Get-ChildItem .\users\*.json -Exclude example.json)) {
+foreach ($account in $(Get-ChildItem .\users\* -Exclude example.json)) {
     iex "node report.js '$account' $steamId"
 
     $counter++
