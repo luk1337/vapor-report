3b381479d1269f049748e2f9243c32a0391087b5
LuK1337 <priv.luk@gmail.com>
2017-02-19 23:57:12 +0100

    Add matchId to powershell arguments

--- report.ps1
+++ report.ps1
@@ -1,11 +1,12 @@
 $counter = 0
 $accounts = (Get-ChildItem .\users\* -Exclude example.json).Count
 $steamId = Read-Host -Prompt 'Enter the steamID64 you want to report'
+$matchId = Read-Host -Prompt 'Enter the matchId if you have it'
 
 node protos/updater.js
 
 foreach ($account in $(Get-ChildItem .\users\* -Exclude example.json)) {
-    iex "node report.js '$account' $steamId"
+    iex "node report.js '$account' $steamId $matchId"
 
     $counter++
     echo "[INFO] Status : $counter/$accounts"
